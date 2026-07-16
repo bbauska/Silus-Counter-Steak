83c8b3630c06fe70d35052de38c82318c004939f
Brian Bauska <brianbauska@gmail.com>
2026-07-16 10:59:55 -0700

    Change initial counter value to 20000
    Updated initial counter value from 0 to 20000.

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,8 @@
 /* /js/scripts.js in github Silus-Counter-Steak making silus-counter-steak.bauska.org */
 /* July 13, 2026 = ## (## hours approx)
  */
-let counter = 0;
-/* ##,### from July 13 (## hours - xx day), 
+let counter = 20000;
+/* 20,000 from July 15 (7 hours - 1 day), 
   all times are approximate. */
 
 function count() {
