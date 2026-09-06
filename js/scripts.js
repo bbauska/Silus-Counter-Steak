0ccb01158cd97a2e524920b0223da88366663111
Brian Bauska <brianbauska@gmail.com>
2026-09-05 21:28:23 -0700

    Add 8750 to counter variable
    Updated counter variable to include additional value.

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,14 @@
 /* /js/scripts.js in github Silus-Counter-Steak making silus-counter-steak.bauska.org */
 /* July 13, 2026 = ## (## hours approx)
  */
-let counter = 20000+28000+32000+10000+12200+7300;
+let counter = 20000+28000+32000+10000+12200+7300+8750;
 /* 20,000 from July 15 (7 hours - 1 day), 
    28,000 from July 16 (? hours - 1 day)
    32,000 from July 20 10pm - July 21 3:32am - 1 day)
    10,000 7/24/26
    12,200 8/3/26
    7,300 8/25/26 to 8/26/26
+   8,750 9/5/2026 8:00pm to 9/5/2026 9:28pm
   all times are approximate. */
 
 function count() {
