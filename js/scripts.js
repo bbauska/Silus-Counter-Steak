19753f70d988cbe6d8d431677be9bfdad390e083
Brian Bauska <brianbauska@gmail.com>
2026-09-12 20:48:21 -0700

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,7 @@
 /* /js/scripts.js in github Silus-Counter-Steak making silus-counter-steak.bauska.org */
 /* July 13, 2026 = ## (## hours approx)
  */
-let counter = 20000+28000+32000+10000+12200+7300+8750+21750+60000+50000;
+let counter = 20000+28000+32000+10000+12200+7300+8750+21750+60000+50000+11119;
 /* 20,000 from July 15 (7 hours - 1 day), 
    28,000 from July 16 (? hours - 1 day)
    32,000 from July 20 10pm - July 21 3:32am - 1 day)
@@ -12,6 +12,7 @@ let counter = 20000+28000+32000+10000+12200+7300+8750+21750+60000+50000;
    21,750 9/5/2026 5 hours 12am-5am
    60,000 9/6/26 7p - 5am 9/7/26
    50,000 9/11/26 5pm - 3am 
+   11,119 9/12/26 6 - 7:30pm
   all times are approximate. */
 
 function count() {
