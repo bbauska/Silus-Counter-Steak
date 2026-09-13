23f455257697baec417ab492a5a6e6f8c7c5ae1e
Brian Bauska <brianbauska@gmail.com>
2026-09-13 22:55:24 +0000

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,18 @@
 /* /js/scripts.js in github Silus-Counter-Steak making silus-counter-steak.bauska.org */
-/* July 13, 2026 = ## (## hours approx)
- */
+/* July 13, 2026 = ## (## hours approx) */
 let counter = 20000+28000+32000+10000+12200+7300+8750+21750+60000+50000+11119+38881;
 /* 20,000 from July 15 (7 hours - 1 day), 
    28,000 from July 16 (? hours - 1 day)
    32,000 from July 20 10pm - July 21 3:32am - 1 day)
-   10,000 7/24/26
-   12,200 8/3/26
-   7,300 8/25/26 to 8/26/26
-   8,750 9/5/2026 8:00pm to 9/5/2026 9:28pm
-   21,750 9/5/2026 5 hours 12am-5am
-   60,000 9/6/26 7p - 5am 9/7/26
-   50,000 9/11/26 5pm - 3am 
-   11,119 9/12/26 6 - 7:30pm
-   38,881 9/13/26 6pm - 4am
+   10,000 7/24/26 90,000 running total
+   12,200 8/3/26 102,200 to date running total
+   7,300 8/25/26 to 8/26/26 109,500 to date
+   8,750 9/5/2026 8:00pm to 9/5/2026 9:28pm 118,250 running total
+   21,750 9/5/2026 5 hours 12am-5am 140,000 running total
+   60,000 9/6/26 7p - 5am 9/7/26 200,000 running total
+   50,000 9/11/26 5pm - 3am  250,000 running total
+   11,119 9/12/26 6 - 7:30pm 261,119 running total
+   38,881 9/13/26 6pm - 4am 300,000 running total
   all times are approximate. */
 
 function count() {
