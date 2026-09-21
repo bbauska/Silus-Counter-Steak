030265ed81465066b17e10740a1be2b8b5834c89
Brian Bauska <brianbauska@gmail.com>
2026-09-20 17:40:12 -0700

    Fix confetti text content assignment

--- js/Confetti.js
+++ js/Confetti.js
@@ -1,8 +1,8 @@
 const confettiContainer = document.querySelector('#confetti-container');
 const showConfetti = () => {
   const confetti = document.createElement('div');
-  confetti.textContent = '🥩';
   confetti.classList.add('confetti');
+  confetti.textContent = '🥩';
   innerWidth = innerWidth * 0.5;
   confetti.style.left = Math.random() * innerWidth + 'px';
   confettiContainer.appendChild(confetti);
