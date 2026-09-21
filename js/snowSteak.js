b4e91bfdc1f4b5c938fe49bc163fb2f92d351285
Brian Bauska <brianbauska@gmail.com>
2026-09-20 19:05:52 -0700

    Expand emoji array for snowflakes
    Added more fun emojis to the snowflake substitution array.

--- js/snowSteak.js
+++ js/snowSteak.js
@@ -25,7 +25,7 @@ const period = [
 ];
 
 //Emojis to substitute for snowflakes, just for fun
-const fun = ['❤️', '😻', '🥩', '🐔', '💘'];
+const fun = ['❤️', '😻', '🥩', '🐔', '💘', '🎯','🔫','🎮','♠','👓','🎤'];
 
 //The CSS styles for the snowflakes and container
 const cssString = `.snowfall-container {
